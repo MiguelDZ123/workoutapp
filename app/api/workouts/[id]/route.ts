73f35ffa672eb7113c673bee7028bda17932caee
MiguelDZ123 <migui.manda1@gmail.com>
2025-03-20 20:34:18 -0400

    Now?

--- app/api/workouts/[id]/route.ts
+++ app/api/workouts/[id]/route.ts
@@ -100,11 +100,11 @@ export async function PATCH(
 
 export async function DELETE(
   request: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }>}
 ) {
   try {
     const session = await getServerSession();
-    const { id } = params;
+    const { id } = await params;
     
     if (!session?.user?.email) {
       return NextResponse.json(
