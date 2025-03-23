b3564730390abf2fe3b3dda8717acc4a4f891950
MiguelDZ123 <migui.manda1@gmail.com>
2025-03-23 17:36:20 -0400

    Added GitHub

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="w-full border-t border-gray-200 dark:border-gray-800 pt-4">
-      <div className="flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between items-center">
+      <div className="max-w-7xl mx-auto flex flex-col sm:flex-row gap-4 sm:gap-0 justify-between items-center">
         <div className="flex items-center gap-2">
           <span className="text-sm font-medium">WORKOUT.IO © 2025</span>
         </div>
